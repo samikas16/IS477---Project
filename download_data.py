416b86833084c341980d00c4012c44bb20eaee59
samika1606 <142349236+samika1606@users.noreply.github.com>
2026-05-05 20:20:27 -0500

    formatting

--- download_data.py
+++ download_data.py
@@ -12,7 +12,6 @@ AQI_URL = "https://aqs.epa.gov/aqsweb/airdata/annual_aqi_by_cbsa_2024.zip"
 OUTPUT_FILE = "annual_aqi_by_cbsa_2024.zip"
 
 def download_file(url, filename):
-    print(f"Downloading {filename}...")
 
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -21,8 +20,6 @@ def download_file(url, filename):
         for chunk in response.iter_content(chunk_size=8192):
             if chunk:
                 f.write(chunk)
-
-    print(f"Saved: {filename}")
 
 def main():
     download_file(AQI_URL, OUTPUT_FILE)
